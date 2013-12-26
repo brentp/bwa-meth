5a2ab109f9667427d22e50cbfd7cadd333db45ed
Brent Pedersen <bpederse@gmail.com>
2013-12-26 13:08:16 -0700

    force gzip

--- compare/src/trim.sh
+++ compare/src/trim.sh
@@ -11,7 +11,7 @@ sickle pe -f $R1 -r $R2 \
           -o $O1 -p $O2 \
           -s singles.fastq \
           -t sanger
-gzip $O1 &
-gzip $O2 &
+gzip -f $O1 &
+gzip -f $O2 &
 rm singles.fastq
 wait
