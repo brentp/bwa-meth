9b47e8e2637106d4487b874f53c8a1d3c8c5164e
Brent Pedersen <bpederse@gmail.com>
2013-12-26 14:58:30 -0700

    legend aesthetics

--- compare/src/plot-quals.R
+++ compare/src/plot-quals.R
@@ -1,6 +1,10 @@
+options(stringsAsFactors=FALSE)
 library(ggplot2)
 
 df = read.delim('sim-qual-summary.txt')
+
+df[df$method == "bis1", "method"] = "bismark"
+df = df[df$qual > 0,]
 
 #df$method = rep(c("bismark", "bsmap", "bwa", "gsnap", "last"), 256)
 
@@ -20,7 +24,8 @@ p = ggplot(df, aes(x=off, y=on, by=method)) +
         #           linetype="dashed")
 p = p + ylab("% Reads On Target")
 p = p + xlab("% Reads Off Target")
-p = p + theme(legend.position = c(0.8, 0.4),
+p = p + theme(
+             legend.position = c(0.55, 0.25),
               legend.text=element_text(size=5, lineheight=5),
               axis.text=element_text(size=5),
               axis.title=element_text(size=8),
@@ -28,6 +33,8 @@ p = p + theme(legend.position = c(0.8, 0.4),
 
 #              legend.key.height=5
               )
+p = p + labs(color="")
+p = p + guides(color=guide_legend(ncol=2, title=NULL))
 #print(p)
 ggsave(file='qual-plot-real.eps', units="cm", width=8.6, height=6.3,
     dpi=600)
