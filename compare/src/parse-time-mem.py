420db04e96875d5a39834ffba415a7187f138d7f
Brent Pedersen <bpederse@gmail.com>
2013-12-26 13:21:49 -0700

    script to make table from lsf logs

---
+++ compare/src/parse-time-mem.py
@@ -0,0 +1,52 @@
+
+def parse_time(log_file):
+
+    for line in open(log_file):
+        if "CPU time" in line:
+            _, time = [x.strip() for x in line.split(":")]
+            time, unit = time.split()
+            assert unit == "sec."
+            return float(time)
+
+def parse_mem(log_file):
+
+    for line in open(log_file):
+        if "Max Memory" in line:
+            _, mem = [x.strip() for x in line.split(":")]
+            mem, unit = mem.split()
+            assert unit == "MB"
+            return float(mem)
+
+def parse_prog(log_file):
+    import os.path as op
+    p = op.basename(log_file)
+    if p.startswith('trim-'):
+        p = p[5:]
+    return p.split("-")[0]
+
+def parse_sim_real(log_file):
+    import os.path as op
+    p = op.basename(log_file)
+    if p.startswith('trim-'):
+        p = p[5:]
+    return p.split("-")[1].split(".")[0]
+
+def parse_trim(log_file):
+    return ["no", "yes"][int("trim" in log_file)]
+
+
+fmt = "{trimmed}\t{program}\t{time}\t{mem}\t{dataset}"
+
+def main(logs):
+    print fmt.replace("{", "").replace("}", "")
+    for log in logs:
+        trimmed = parse_trim(log)
+        mem = parse_mem(log)
+        time = parse_time(log)
+        program = parse_prog(log)
+        dataset = parse_sim_real(log)
+        print fmt.format(**locals())
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1:])
