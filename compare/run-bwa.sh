44b0ed0c4812cff4ca3f701809d51f17b9864a10
Brent Pedersen <bpederse@gmail.com>
2013-12-28 10:44:42 -0700

    change to new script name

--- compare/run-bwa.sh
+++ compare/run-bwa.sh
@@ -1,7 +1,7 @@
 . ./common.sh
 
 rm -f logs/bwa-$name.err logs/bwa-$name.out
-echo "python ../bwa-meth.py --reference $REF \
+echo "python ../bwameth.py --reference $REF \
         -t 22 -p results/bwa-$name $FQ1 $FQ2" \
         | bsub -J bwa-$name \
                -e logs/bwa-$name.err \
@@ -9,7 +9,7 @@ echo "python ../bwa-meth.py --reference $REF \
 
 
 rm -f logs/trim-bwa-$name.err logs/trim-bwa-$name.out
-echo "python ../bwa-meth.py --reference $REF \
+echo "python ../bwameth.py --reference $REF \
         -t 22 -p results/trim/bwa-$name $TRIM_FQ1 $TRIM_FQ2" \
         | bsub -J trim-bwa-$name \
                -e logs/trim-bwa-$name.err \
