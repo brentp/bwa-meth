eb7d1a0cbf2fdc83de01a9c9d18ab6a29a834fa0
Brent Pedersen <bpederse@gmail.com>
2014-02-11 13:04:22 -0700

    commit non-working gnumap-bs

--- compare/run-gnumapbs.sh
+++ compare/run-gnumapbs.sh
@@ -1,15 +1,27 @@
 . ./common.sh
 
+module load gnumap/3.0.2
+
+export GNUMAPBS=/home/brentp/src/gnumap/gnumaps
+PATH=$PATH:$GNUMAPBS/bin:$GNUMAPBS/scripts/
+export PATH
 # taken from gnumap-bs paper
-cmd="gnumap -b -m 17 -s 1 -T 20 -a 0.90 -b -G $REF --illumina ???? -o"
+cmd="gnumaps.pl --genome $REF --lib_type wt1 --acc 0.9 --nt_conv bs --num_threads 8 --outdir"
+
+zless $FQ1 > $$.r1.tmp
+zless $FQ2 > $$.r2.tmp
 
 rm -f logs/gnumap-$name.err logs/gnumap-$name.out
-echo "$cmd results/gnumapbs-$name.sam" | bsub -J gnumap-$name \
+echo "$cmd results/gnumapbs-$name/ --pair_1 $$.r1.tmp --pair_2 $$.r2.tmp" \
+    | bsub -J gnumap-$name \
                  -e logs/gnumap-$name.err \
                  -o logs/gnumap-$name.out -n 8
 
+exit;
 
 rm -f logs/trim-gnumap-$name.err logs/trim-gnumap-$name.out
-echo "$cmd results/gnumapbs-$name.sam" | bsub -J trim-gnumap-$name \
+echo "$cmd results/trim/gnumapbs-$name/ --pair_1 <(zless $TRIM_FQ1) --pair_2 <(zless $TRIM_FQ2)" \
+    | bsub -J trim-gnumap-$name \
                  -e logs/trim-gnumap-$name.err \
                  -o logs/trim-gnumap-$name.out -n 8
+
