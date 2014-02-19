b48b1151f061f95c1dc3f71f89b46af0827dc486
Brent Pedersen <bpederse@gmail.com>
2014-02-19 13:03:11 -0700

    use tmp file for gnumapbs

--- compare/run-gnumapbs.sh
+++ compare/run-gnumapbs.sh
@@ -2,8 +2,8 @@
 
 module load gnumap/3.0.2
 
-export GNUMAPBS=/home/brentp/src/gnumap/gnumaps
-PATH=$PATH:$GNUMAPBS/bin:$GNUMAPBS/scripts/
+export GNUMAPS=/home/brentp/src/gnumap/gnumaps
+PATH=$PATH:$GNUMAPS/bin:$GNUMAPS/scripts/
 export PATH
 # taken from gnumap-bs paper
 cmd="gnumaps.pl --genome $REF --lib_type wt1 --acc 0.9 --nt_conv bs --num_threads 8 --outdir"
@@ -20,7 +20,7 @@ echo "$cmd results/gnumapbs-$name/ --pair_1 $$.r1.tmp --pair_2 $$.r2.tmp" \
 exit;
 
 rm -f logs/trim-gnumap-$name.err logs/trim-gnumap-$name.out
-echo "$cmd results/trim/gnumapbs-$name/ --pair_1 <(zless $TRIM_FQ1) --pair_2 <(zless $TRIM_FQ2)" \
+echo "$cmd results/trim/gnumapbs-$name/ --pair_1 $$.r1.tmp --pair_2 $$.r2.tmp" \
     | bsub -J trim-gnumap-$name \
                  -e logs/trim-gnumap-$name.err \
                  -o logs/trim-gnumap-$name.out -n 8
