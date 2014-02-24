22d4b1bf5ebd0c4603a2d088af3dcde37bb42266
Brent Pedersen <bpederse@gmail.com>
2014-02-24 12:19:42 -0700

    update bison run script

--- compare/run-bison.sh
+++ compare/run-bison.sh
@@ -1,19 +1,32 @@
 . ./common.sh
 
+PATH=$PATH:/opt/mpich2/gnu/bin/
 
-cmd="bison -N 1 --very-sensitive -p 12 -o"
-name=bison
+GEN=$(dirname $REF)/
+cmd="bison --directional -N 1 --very-sensitive -p 12 -g $GEN -o"
+prog=bison
 
-rm -f logs/$name.err logs/$name.out
-echo "$cmd results/$name -1 $FQ1 -2 $FQ2" \
-        | bsub -J $name \
-               -e logs/$name.err \
-               -o logs/$name.out -n 12
+rm -rf results/$prog/
+mkdir -p results/$prog/
+rm -f logs/$prog-$name.err logs/$prog-$name.out
 
+echo "mpiexec $cmd results/$prog/ -1 $FQ1 -2 $FQ2" \
+        | bsub -J $prog-$name \
+               -R "span[ptile=5]" \
+               -a openmpi \
+               -q shared \
+               -e logs/$prog-$name.err \
+               -o logs/$prog-$name.out -n 5
+exit
 
-rm -f logs/trim-$name.err logs/trim-$name.out
+rm -rf results/trim/$prog/
+mkdir -p results/trim/$prog/
+rm -f logs/trim-$prog-$name.err logs/trim-$prog-$name.out
 
-echo "$cmd results/trim/$name -1 $TRIM_FQ1 -2 $TRIM_FQ2" \
-        | bsub -J trim-$name \
-               -e logs/trim-$name.err \
-               -o logs/trim-$name.out -n 12
+echo "mpiexec $cmd results/trim/$prog/ -1 $TRIM_FQ1 -2 $TRIM_FQ2" \
+        | bsub -J trim-$prog-$name \
+               -R "span[ptile=12]" \
+               -a mvapich \
+               -q shared \
+               -e logs/trim-$prog-$name.err \
+               -o logs/trim-$prog-$name.out -n 60
