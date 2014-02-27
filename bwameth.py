966bee52248c87f636e9f2eabd5a21cde8407f47
Brent Pedersen <bpederse@gmail.com>
2014-02-27 12:55:25 -0700

    v0.08

--- bwameth.py
+++ bwameth.py
@@ -30,7 +30,7 @@ except ImportError: # python3
     maketrans = str.maketrans
 from toolshed import nopen, reader, is_newer_b
 
-__version__ = "0.07"
+__version__ = "0.08"
 
 def checkX(cmd):
     for p in os.environ['PATH'].split(":"):
