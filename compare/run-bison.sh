5377ee51d5c1420588809180a39c17cd58f739d2
Brent Pedersen <bpederse@gmail.com>
2014-02-27 14:24:35 -0700

    working bison invocation (using correct mpi)

--- compare/run-bison.sh
+++ compare/run-bison.sh
@@ -1,9 +1,12 @@
 . ./common.sh
 
-PATH=$PATH:/opt/mpich2/gnu/bin/
+
+PATH=/opt/mpich2/gnu/bin/:$PATH
 
 GEN=$(dirname $REF)/
-cmd="bison --directional -N 1 --very-sensitive-local -p 12 -g $GEN -o"
+
+
+cmd="bison --directional --score-min 'L,-0.6,-0.6' -N 1 --very-sensitive-local -p 8 -g $GEN -o"
 prog=bison
 
 rm -rf results/$prog/
@@ -11,25 +14,30 @@ mkdir -p results/$prog/
 rm -f logs/$prog-$name.err logs/$prog-$name.out
 
 mv="mv results/$prog/${name}_R1.bam results/$prog-$name.bam"
-echo "mpiexec $cmd results/$prog/ -1 $FQ1 -2 $FQ2; $mv" \
+echo "mpiexec -np 3 $cmd results/$prog/ -1 $FQ1 -2 $FQ2" \
         | bsub -J $prog-$name \
-               -n 3 \
-               -R "span[ptile=5]" \
+               -R "span[ptile=1]" \
                -a mvapich \
                -q shared \
                -e logs/$prog-$name.err \
-               -o logs/$prog-$name.out 
+               -o logs/$prog-$name.out \
+               -n 3
+
+echo $mv | bsub -w "done($prog-$name)" -e /dev/null -o /dev/null -J mv
+
 
 rm -rf results/trim/$prog/
 mkdir -p results/trim/$prog/
 rm -f logs/trim-$prog-$name.err logs/trim-$prog-$name.out
 
 mv="mv results/trim/$prog/${name}_R1.bam results/trim/$prog-$name.bam"
-echo "mpiexec $cmd results/trim/$prog/ -1 $TRIM_FQ1 -2 $TRIM_FQ2; $mv" \
+echo "mpiexec -np 3 $cmd results/trim/$prog/ -1 $TRIM_FQ1 -2 $TRIM_FQ2" \
         | bsub -J trim-$prog-$name \
-               -n 3 \
                -R "span[ptile=5]" \
                -a mvapich \
                -q shared \
                -e logs/trim-$prog-$name.err \
                -o logs/trim-$prog-$name.out
+               -n 3
+
+echo $mv | bsub -w "done(trim-$prog-$name)" -e /dev/null -o /dev/null -J mv-trim
