15f90cb0bc61ef78cb986967d41abe957a2b07e3
Brent Pedersen <bpederse@gmail.com>
2014-02-28 12:56:02 -0700

    fix names in simulation

--- compare/src/gen-simulated.sh
+++ compare/src/gen-simulated.sh
@@ -1,15 +1,18 @@
 . ./common.sh
 
+#    -n 1000000 \
 cd data/
 Sherman \
     --genome_folder $BIS_REF \
     --length 100 \
-    -n 1000000 \
+    -n 100000 \
     -pe \
-    -X 800 \
+    -X 500 \
     --CG_conversion 25 \
     --CH_conversion 95 \
     --error_rate 1
 
-mv simulated_1.fastq sim_R1.fastq && gzip -f sim_R1.fastq
-mv simulated_2.fastq sim_R2.fastq && gzip -f sim_R2.fastq
+sed 's/_R1$//' simulated_1.fastq | gzip -c > sim_R1.fastq.gz &
+sed 's/_R2$//' simulated_2.fastq | gzip -c > sim_R2.fastq.gz
+wait
+rm -f simulated_1.fastq simulated_2.fastq
