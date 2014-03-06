c624dc88a6caa471ea456b5f1f1e8aa0dfe94d80
Brent Pedersen <bpederse@gmail.com>
2014-03-06 15:30:02 -0700

    plot-quals update

--- compare/src/plot-quals.R
+++ compare/src/plot-quals.R
@@ -2,6 +2,8 @@
 options(stringsAsFactors=FALSE)
 library(ggplot2)
 
+#X11.options(antialias="subpixel", type="cairo")
+
 args = commandArgs(TRUE)
 
 df = read.delim(args[1])
@@ -13,6 +15,7 @@ df[df$method == "bwar", "method"] = "bwa-strand"
 df[df$method == "bwa", "method"] = "bwameth"
 
 df[grep("sim_R1", df$method, fixed=TRUE), "method"] = "bison"
+df[grep("real_R1", df$method, fixed=TRUE), "method"] = "bison"
 df$method = factor(df$method)#, levels=c("last", "bsmap", "gsnap", "bwa", "bismark", "bwa-strand", "bsmooth", "bison"))
 df = df[df$qual > 0,]
 
@@ -26,12 +29,16 @@ require("grid")
 
 
 p = ggplot(df, aes(x=off, y=on, by=method)) +
-         geom_point(aes(color=method), size=1.3) +
+         geom_point(color="grey60", size=1.26) +
+         geom_point(aes(color=method), size=1.0) +
          scale_shape(solid = FALSE) +
          geom_line(aes(color=method), linetype="dashed", linewidth=0.1, size=0.1) 
 p = p + ylab("% Reads On Target")
 p = p + xlab("% Reads Off Target")
-p = p + xlim(xmin=0, xmax=1.5)
+if(any(grep("sim", args))){
+    p = p + xlim(xmin=0, xmax=1.5)
+    p = p + ylim(ymin=65, xmax=92)
+}
 #p = p + theme_bw()
 p = p + theme(
              #legend.position = c(0.55, 0.25),
