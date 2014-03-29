1375ced0534165bf04c06b67731726920612db67
Brent Pedersen <bpederse@gmail.com>
2014-03-29 09:14:31 -0600

    version getter in setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,25 @@ import ez_setup
 ez_setup.use_setuptools()
 from setuptools import setup
 
-import bwameth
+# from mpld3
+def get_version():
+    """Get the version info from the mpld3 package without importing it"""
+    import ast
+
+    with open("bwameth.py") as init_file:
+        module = ast.parse(init_file.read())
+
+    version = (ast.literal_eval(node.value) for node in ast.walk(module)
+               if isinstance(node, ast.Assign)
+               and node.targets[0].id == "__version__")
+    try:
+        return next(version)
+    except StopIteration:
+        raise ValueError("version could not be located")
+
 
 setup(name='bwameth',
-      version=bwameth.__version__,
+      version=get_version(),
       description="align BS-Seq reads with bwa mem",
       #packages=['.'],
       author="Brent Pedersen",
