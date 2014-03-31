be9cb110450d22a8f4f0bb376fdb2a976169b8a6
Brent Pedersen <bpederse@gmail.com>
2014-03-31 12:52:21 -0600

    update gen-simulated to have e coli data

--- compare/src/gen-simulated.sh
+++ compare/src/gen-simulated.sh
@@ -1,6 +1,7 @@
 . ./common.sh
 
 cd data/
+
 Sherman \
     --genome_folder $BIS_REF \
     --length 100 \
@@ -13,5 +14,24 @@ Sherman \
 
 sed 's/_R1$//' simulated_1.fastq | gzip -c > sim_R1.fastq.gz &
 sed 's/_R2$//' simulated_2.fastq | gzip -c > sim_R2.fastq.gz
+
+# wget -O data/e_coli_k12_mg1655.fa "http://www.ncbi.nlm.nih.gov/nuccore/NC_000913.3?report=fasta&log$=seqview&format=text"
+# and remove html
 wait
+
+rm -f simulated_1.fastq simulated_2.fastq
+
+Sherman \
+    --genome_folder . \
+    --length 100 \
+    -n 100000 \
+    -pe \
+    -X 500 \
+    --CG_conversion 98 \
+    --CH_conversion 98 \
+    --error_rate 1
+
+sed 's/_R1$/_BAD/' simulated_1.fastq | gzip -c >> sim_R1.fastq.gz &
+sed 's/_R2$/_BAD/' simulated_2.fastq | gzip -c >> sim_R2.fastq.gz
+
 rm -f simulated_1.fastq simulated_2.fastq
