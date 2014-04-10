1caeaf9dde7356968d65dd0543ff6f14cad61c8b
Brent Pedersen <bpederse@gmail.com>
2014-04-09 20:46:05 -0600

    force int mapq

--- bwameth.py
+++ bwameth.py
@@ -347,7 +347,7 @@ def handle_reads(alns, set_as_failed):
         if aln.longest_match() < (len(orig_seq) * 0.44):
             aln.flag |= 0x200  # fail qc
             aln.flag &= (~0x2) # un-pair
-            aln.mapq = min(aln.mapq, 1)
+            aln.mapq = min(int(aln.mapq), 1)
 
         mate_direction = aln.chrom_mate[0]
         if mate_direction not in "*=":
