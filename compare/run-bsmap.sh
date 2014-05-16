e4564c11701344a97c0cea948dcdce736cc628d1
Brent Pedersen <bpederse@gmail.com>
2014-05-16 12:39:10 -0600

    bsmap default parameters

--- compare/run-bsmap.sh
+++ compare/run-bsmap.sh
@@ -2,6 +2,8 @@
 
 cmd="bsmap -u -a $FQ1 -b $FQ2 -d $REF -o results/bsmap-$name.bam -v 3 \
              -p 8 -m 0 -x 1000 -S 42 -n 0 -s 12 -I 1"
+# run bsmap with default parameters
+cmd="bsmap -a $FQ1 -b $FQ2 -d $REF -o results/defaultbsmap-$name.bam -p 8 "
 
 rm -f logs/bsmap-$name.err logs/bsmap-$name.out
 echo $cmd | bsub -J bsmap-$name \
@@ -12,6 +14,9 @@ echo $cmd | bsub -J bsmap-$name \
 
 cmd="bsmap -u -a $TRIM_FQ1 -b $TRIM_FQ2 -d $REF -o results/trim/bsmap-$name.bam -v 3 \
              -p 8 -m 0 -x 1000 -S 42 -n 0 -s 12 -I 1"
+
+# run bsmap with default parameters
+cmd="bsmap -a $TRIM_FQ1 -b $TRIM_FQ2 -d $REF -o results/trim/defaultbsmap-$name.bam -p 8 "
 
 rm -f logs/trim-bsmap-$name.err logs/trim-bsmap-$name.out
 echo $cmd | bsub -J trim-bsmap-$name \
