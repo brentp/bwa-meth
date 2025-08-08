4e04966a177f90e1bb15790e162cca06b3c216de
Brent Pedersen <bpederse@gmail.com>
2025-08-08 09:02:17 -0700

    bump version

--- bwameth.py
+++ bwameth.py
@@ -46,7 +46,7 @@ except ImportError: # python3
 import toolshed
 from toolshed import nopen, reader, is_newer_b
 
-__version__ = "0.2.7"
+__version__ = "0.2.9"
 
 def nopen_keep_parent_stdin(f, mode="r"):
 
