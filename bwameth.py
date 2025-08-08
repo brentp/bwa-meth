d5a7d05dfea06cbb626db5f14523e69ba52bcf7c
Michael Beavitt <mbeavitt@gmail.com>
2025-08-08 09:59:05 +0000

    whitespace formatting

--- bwameth.py
+++ bwameth.py
@@ -354,12 +354,12 @@ def bwa_mem(fa, fq_convert_cmd, extra_args, threads=1, rg=None,
             idx = "mem1"
             sys.stderr.write("--------------------\n")
             sys.stderr.write("Found BWA MEM index\n")
-            
+
         elif is_newer_b(conv_fa, (conv_fa + '.amb', conv_fa + '.pac')):
             idx = "mem2"
             sys.stderr.write("---------------------\n")
             sys.stderr.write("Found BWA MEM2 index\n")
-            
+
         else:
             raise BWAMethException("first run bwameth.py index %s OR bwameth.py index-mem2 %s OR make sure the modification time on the generated c2t files is newer than on the .fa file" % (fa, fa))
 
