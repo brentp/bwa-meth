87567e8f46f82e7568d2dcff9389c06188d13719
Matt McGuffie <mmcguffie@twistbioscience.com>
2026-06-02 19:36:39 +0000

    ∆ version bump

--- bwameth.py
+++ bwameth.py
@@ -46,7 +46,7 @@ except ImportError: # python3
 import toolshed
 from toolshed import nopen, reader, is_newer_b
 
-__version__ = "0.2.9"
+__version__ = "0.3.0"
 
 def nopen_keep_parent_stdin(f, mode="r"):
 
